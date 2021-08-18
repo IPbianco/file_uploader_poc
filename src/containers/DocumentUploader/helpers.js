6eab4cf7f1ea7d666e0fba3bec4cc9a6efe825bb
IPbianco <ignacio@pensionbee.com>
2021-08-18 17:21:34 +0100

    Posting to bin request

--- src/containers/DocumentUploader/helpers.js
+++
@@ -1,6 +0,0 @@
-export const uploadFile = file => {
-  console.log('*************');
-  console.log('FIlE:');
-  console.log(file);
-  console.log('*************');
-};
--- src/containers/DocumentUploader/index.js
+++ src/containers/DocumentUploader/index.js
@@ -2,7 +2,8 @@ import React, {PureComponent} from 'react';
 import {View, Text} from 'react-native';
 
 import DocumentPicker from 'react-native-document-picker';
-import {uploadFile} from './helpers.js';
+
+const uploadDocumentUrl = 'https://enf1z5o26le0d6s.m.pipedream.net'; // Change to your own bin request
 
 export class DocumentUploaderScreen extends PureComponent {
   async openFilepicker() {
@@ -11,7 +12,11 @@ export class DocumentUploaderScreen extends PureComponent {
         type: [DocumentPicker.types.images, DocumentPicker.types.pdf],
       });
       const {uri, type, name, size} = res[0];
-      uploadFile({uri, type, name, size});
+      const body = {uri, type, name, size};
+      await fetch(uploadDocumentUrl, {
+        method: 'post',
+        body,
+      });
     } catch (err) {
       if (DocumentPicker.isCancel(err)) {
         // User cancelled the picker, exit any dialogs or menus and move on
