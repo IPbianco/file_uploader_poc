72c6fc451fe965a750c348b0de55195ce5123f47
IPbianco <ignacio@pensionbee.com>
2021-08-18 16:38:03 +0100

    Adds basic functionality

---
+++ src/containers/DocumentUploader/helpers.js
@@ -0,0 +1,6 @@
+export const uploadFile = file => {
+  console.log('*************');
+  console.log('FIlE:');
+  console.log(file);
+  console.log('*************');
+};
---
+++ src/containers/DocumentUploader/index.js
@@ -0,0 +1,33 @@
+import React, {PureComponent} from 'react';
+import {View, Text} from 'react-native';
+
+import DocumentPicker from 'react-native-document-picker';
+import {uploadFile} from './helpers.js';
+
+export class DocumentUploaderScreen extends PureComponent {
+  async openFilepicker() {
+    try {
+      const res = await DocumentPicker.pick({
+        type: [DocumentPicker.types.images],
+      });
+      const {uri, type, name, size} = res[0];
+      uploadFile({uri, type, name, size});
+    } catch (err) {
+      if (DocumentPicker.isCancel(err)) {
+        // User cancelled the picker, exit any dialogs or menus and move on
+      } else {
+        throw err;
+      }
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Text onPress={() => this.openFilepicker()}>Hello world!</Text>
+      </View>
+    );
+  }
+}
+
+export default DocumentUploaderScreen;
